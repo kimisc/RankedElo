281f0d277b6b4f74ddcc735d8a68269c5bfb749f
kimisc <kimarp15@gmail.com>
2018-11-11 13:51:21 +0200

    initial commit

---
+++ RankedElo.Core.Tests/Entities/MatchTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace RankedElo.Core.Tests
+{
+    public class MatchTests
+    {
+
+    }
+}
---
+++ RankedElo.Core/Entities/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RankedElo.Core.Model
+{
+    class Player
+    {
+    }
+}
